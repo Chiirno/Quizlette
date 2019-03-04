33d2144da495e763ae3a16cb2f53432dd292d3ac
Cirno <GeniusChiruno@gmail.com>
2019-03-04 18:11:46 -0400

    added fade in/out + refactoring

--- Quizlette/public_html/quizlette.js
+++ Quizlette/public_html/quizlette.js
@@ -29,27 +29,68 @@ function addThird(){
     if (round===3){score += 9;}
 }
 
+function fadeOut(){
+    
+    sleep();
+    setTimeout(nextRound, 700);
+    setTimeout(awaken, 700);
+    
+}
+
+function sleep(){
+    var leftBut = document.getElementById("buttonLeft");
+    var midBut = document.getElementById("buttonMid");
+    var rightBut = document.getElementById("buttonRight");
+        
+    fade(leftBut);
+    fade(midBut);
+    fade(rightBut);
+}
+function awaken(){
+    var leftBut = document.getElementById("buttonLeft");
+    var midBut = document.getElementById("buttonMid");
+    var rightBut = document.getElementById("buttonRight");
+        
+    fadeIn(leftBut);
+    fadeIn(midBut);
+    fadeIn(rightBut);
+}
+
 function nextRound(){
+    
+    var leftBut = document.getElementById("buttonLeft");
+    var midBut = document.getElementById("buttonMid");
+    var rightBut = document.getElementById("buttonRight");
+
     if (round===1){
-        document.getElementById("choice1").innerHTML = "Four";
-        document.getElementById("choice2").innerHTML = "Five";
-        document.getElementById("choice3").innerHTML = "Six";
+        leftBut.innerHTML = "Four";
+        midBut.innerHTML = "Five";
+        rightBut.innerHTML = "Six";
         document.getElementById("round").innerHTML = "Pick your second number!";
         round = 2;
     } else if (round===2){
-        document.getElementById("choice1").innerHTML = "Seven";
-        document.getElementById("choice2").innerHTML = "Eight";
-        document.getElementById("choice3").innerHTML = "Nine";
+        leftBut.innerHTML = "Seven";
+        midBut.innerHTML = "Eight";
+        rightBut.innerHTML = "Nine";
         document.getElementById("round").innerHTML = "Pick your third number!";
         round = 3;
     } else if (round===3){
-        document.getElementById("choice1").innerHTML = "Game";
-        document.getElementById("choice2").innerHTML = "is";
-        document.getElementById("choice3").innerHTML = "done!";
+        leftBut.innerHTML = "Game";
+        midBut.innerHTML = "is";
+        rightBut.innerHTML = "done!";
         document.getElementById("round").innerHTML = "Thats all folks";
         round = 0;
     }
 }
+
+
+
+function newRound(){
+    document.getElementById("buttonLeft").innerHTML = "";
+    document.getElementById("buttonMiddle").innerHTML = "";
+    document.getElementById("ButtonRight").innerHTML = "";
+}
+
 
 function displayScore(){
     document.getElementById("tally").innerHTML = score;
@@ -71,3 +112,31 @@ $(document).ready(function(){
 });         
 
  */
+
+
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.01){
+            clearInterval(timer);
+            element.style.visibility = 'hidden';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 1 + ")";
+        op -= 0.5;
+    }, 10);
+}
+
+function fadeIn(element) {
+    var op = 0.1;  // initial opacity
+    element.style.visibility = 'visible';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
