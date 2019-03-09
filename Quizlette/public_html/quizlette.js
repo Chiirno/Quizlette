ce33a83a6c6c4ff210ad5241b17971a6dd75054e
Cirno <GeniusChiruno@gmail.com>
2019-03-08 23:01:11 -0400

    some changes im not sure

--- Quizlette/public_html/quizlette.js
+++ Quizlette/public_html/quizlette.js
@@ -5,94 +5,111 @@
  */
 
 var x = 0;
-
 var round = 1;
 var score = 0;
+var curRound = "";
+var answerSheet = [];
 
 
-
-function addFirst(){
-    if (round===1){score += 1;}
-    if (round===2){score += 4;}
-    if (round===3){score += 7;}
-}
-
-function addSecond(){
-    if (round===1){score += 2;}
-    if (round===2){score += 5;}
-    if (round===3){score += 8;}
-}
-
-function addThird(){
-    if (round===1){score += 3;}
-    if (round===2){score += 6;}
-    if (round===3){score += 9;}
-}
-
-function fadeOut(){
-    
+function fadeOut() {
     sleep();
-    setTimeout(nextRound, 700);
+
+    var leftBut = document.getElementById("buttonLeft");
+    var midBut = document.getElementById("buttonMid");
+    var rightBut = document.getElementById("buttonRight");
+
+    if (round === 5) {
+        document.getElementById("round").innerHTML = "that's all folks" + answerSheet;
+        leftBut.style.visibility = "hidden";
+        midBut.style.visibility = "hidden";
+        rightBut.style.visibility = "hidden";
+    } else {
+        setTimeout(nextQuestion, 700);
+    }
     setTimeout(awaken, 700);
-    
+    round++;
 }
 
-function sleep(){
+function sleep() {
+    fade(document.getElementById("Qtip"));
+}
+function awaken() {
+    fadeIn(document.getElementById("Qtip"));
+}
+
+
+function leftChoice() {
+    answerSheet.push(["left", curRound]);
+}
+
+function middleChoice() {
+    answerSheet.push(["middle", curRound]);
+}
+
+function rightChoice() {
+    answerSheet.push(["right", curRound]);
+}
+
+function nextQuestion() {
     var leftBut = document.getElementById("buttonLeft");
     var midBut = document.getElementById("buttonMid");
     var rightBut = document.getElementById("buttonRight");
-        
-    fade(leftBut);
-    fade(midBut);
-    fade(rightBut);
-}
-function awaken(){
-    var leftBut = document.getElementById("buttonLeft");
-    var midBut = document.getElementById("buttonMid");
-    var rightBut = document.getElementById("buttonRight");
-        
-    fadeIn(leftBut);
-    fadeIn(midBut);
-    fadeIn(rightBut);
-}
 
-function nextRound(){
-    
-    var leftBut = document.getElementById("buttonLeft");
-    var midBut = document.getElementById("buttonMid");
-    var rightBut = document.getElementById("buttonRight");
+    var r = Math.floor((Math.random() * 3) + 1);
 
-    if (round===1){
-        leftBut.innerHTML = "Four";
-        midBut.innerHTML = "Five";
-        rightBut.innerHTML = "Six";
-        document.getElementById("round").innerHTML = "Pick your second number!";
-        round = 2;
-    } else if (round===2){
-        leftBut.innerHTML = "Seven";
-        midBut.innerHTML = "Eight";
-        rightBut.innerHTML = "Nine";
-        document.getElementById("round").innerHTML = "Pick your third number!";
-        round = 3;
-    } else if (round===3){
-        leftBut.innerHTML = "Game";
-        midBut.innerHTML = "is";
-        rightBut.innerHTML = "done!";
-        document.getElementById("round").innerHTML = "Thats all folks";
-        round = 0;
+    switch (r) {
+
+        case 1: //Hydrogen
+            curRound = "Hydrogen";
+            document.getElementById("round").innerHTML = "How many protons does a Hydrogen atom have?";
+            leftBut.innerHTML = "One";
+            midBut.innerHTML = "Zero";
+            rightBut.innerHTML = "Two";
+            break;
+
+        case 2://Helium
+            curRound = "Helium";
+            document.getElementById("round").innerHTML = "How many protons does a Helium atom have?";
+            leftBut.innerHTML = "Twenty";
+            midBut.innerHTML = "Two";
+            rightBut.innerHTML = "Two";
+            break;
+
+        case 3: //Oxygen
+            curRound = "Oxygen";
+            document.getElementById("round").innerHTML = "How many protons does a Oxygen atom have?";
+            leftBut.innerHTML = "Four";
+            midBut.innerHTML = "Nine";
+            rightBut.innerHTML = "Eight";
+            break;
+
+        case 4: //Carbon
+            curRound = "Carbon";
+            document.getElementById("round").innerHTML = "How many protons does a Carbon atom have?";
+            leftBut.innerHTML = "Two";
+            midBut.innerHTML = "Three";
+            rightBut.innerHTML = "Four";
+            break;
+
+        default:
+            document.getElementById("round").innerHTML = r;
+            leftBut.innerHTML = "Zero";
+            midBut.innerHTML = "Zero";
+            rightBut.innerHTML = "Zero";
+
     }
 }
 
 
 
-function newRound(){
+function newRound() {
     document.getElementById("buttonLeft").innerHTML = "";
     document.getElementById("buttonMiddle").innerHTML = "";
     document.getElementById("ButtonRight").innerHTML = "";
 }
 
 
-function displayScore(){
+function displayScore() {
     document.getElementById("tally").innerHTML = score;
     // document.getElementById("cat").src = "kitty.jpg";
     hide(document.getElementById("score"));
@@ -103,36 +120,26 @@ function imageReload() {
 }
 
 
-/*
-
-$(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
-    });
-});         
-
- */
-
 
 
 function fade(element) {
     var op = 1;  // initial opacity
     var timer = setInterval(function () {
-        if (op <= 0.01){
+        if (op <= 0.01) {
             clearInterval(timer);
             element.style.visibility = 'hidden';
         }
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 1 + ")";
         op -= 0.5;
-    }, 10);
+    }, 100);
 }
 
 function fadeIn(element) {
     var op = 0.1;  // initial opacity
     element.style.visibility = 'visible';
     var timer = setInterval(function () {
-        if (op >= 1){
+        if (op >= 1) {
             clearInterval(timer);
         }
         element.style.opacity = op;
